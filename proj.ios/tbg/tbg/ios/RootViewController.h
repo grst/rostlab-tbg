6614ceecfc89ed214cc627f5c68b896f49943407
Alexandro <xxx@Alexandros-Mac.local>
2013-06-11 13:50:05 +0200

    ïos project

---
+++ proj.ios/tbg/tbg/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  tbgAppController.h
+//  tbg
+//
+//  Created by Alexandro on 6/11/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
