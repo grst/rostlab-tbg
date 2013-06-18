cf6d4ac19fd25d657b381392739363ccd88fb536
Sebastian Wilzbach <sebi@wilzbach.me>
2013-06-18 15:38:08 +0200

    box2d included

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -10,12 +10,14 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
                    ../../Classes/HelloWorldScene.cpp
                    
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes                   
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES += $(COCOS2DX_ROOT)/external/Box2D
 
-LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static cocos_extension_static
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static cocos_extension_static box2d_static
             
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,CocosDenshion/android) \
 $(call import-module,cocos2dx) \
-$(call import-module,extensions)
+$(call import-module,extensions) \
+$(call import-module,external/Box2D)
