768ec4f993dc215100b47767760b2aeea2735452
grst <mail@gregor-sturm.de>
2013-10-13 10:13:33 +0200

    .

--- Classes/ui_elements/AminoAcid.h
+++ Classes/ui_elements/AminoAcid.h
@@ -8,7 +8,7 @@
 #ifndef AMINOACID_H_
 #define AMINOACID_H_
 #include "cocos2d.h"
-#include "helper/HelperFunctions.h"
+#include "../helper/HelperFunctions.h"
 
 class AminoAcid: public cocos2d::CCSprite {
 private:
