fec28ed61ac52024ca7ba141e8d7b2ee99118e39
Sebastian Wilzbach <sebi@wilzbach.me>
2013-11-14 00:18:07 +0100

    added globals

---
+++ Classes/Globals.cpp
@@ -0,0 +1,15 @@
+/*
+ * Globals.cpp
+ *
+ *  Created on: Nov 13, 2013
+ *      Author: "Sebastian Wilzbach <sebi@wilzbach.me>"
+ */
+
+#include "Globals.h"
+
+
+// Here we define all global variables
+namespace Globals{
+	int level = 0;
+	int score =0;
+}
---
+++ Classes/Globals.h
@@ -0,0 +1,17 @@
+/*
+ * Globals.h
+ *
+ *  Created on: Nov 13, 2013
+ *      Author: "Sebastian Wilzbach <sebi@wilzbach.me>"
+ */
+
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+
+namespace Globals{
+	const bool gSoundEnabled = true;
+	extern int level;
+	extern int score;
+}
+
+#endif /* CONSTANTS_H_ */
